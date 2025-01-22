0f5e7c24996e48a55b0d0ec37648b1bde0592908
Jinoveva <jinovevalopes@gmail.com>
2025-01-22 13:15:49 +0200

    Created React App

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.shecodes.io/graduates/122713-jinoveva-lopes" target="_blank">
+          Jinoveva Lopes
+          </a>{" "}
+          and is{" "}
+          <a
+            href=""
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href=""
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
