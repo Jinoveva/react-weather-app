61c394c93496f1d71ab609fe02319d9c27db4b7d
Jinoveva <jinovevalopes@gmail.com>
2025-04-01 21:46:29 +0200

    Completed css

--- src/App.js
+++ src/App.js
@@ -6,20 +6,59 @@ import ReactAnimatedWeather from "react-animated-weather";
 export default function App() {
   return (
     <div className="App">
-      <form>
-        <input type="search" placeholder="Enter a city..."></input>
-        <input type="submit" value="Search"></input>
-      </form>
-      <hr></hr>
-      <div>
-        <h1>City</h1>
-        <p>
-          Day Ti:me, description
-          <br />
-          Humidity: ..%, Wind: ..km/h
-        </p>
+      <div className="container">
+        <form>
+          <input
+            id="search-input"
+            type="search"
+            placeholder="Enter a city..."
+          ></input>
+          <input id="submit-input" type="submit" value="Search"></input>
+        </form>
+        <hr id="hr-line"></hr>
+        <div className="weather-box">
+          <div className="weather-summary">
+            <h1>City</h1>
+            <p>
+              Day Ti:me, description
+              <br />
+              Humidity: <span className="weather-info">..%</span>, Wind:{" "}
+              <span className="weather-info">..km/h</span>
+            </p>
+          </div>
+          <div classname="weather-temp">
+            <span className="temp-number">☁️10</span>
+            <span className="degrees-celcius">°C</span>
+          </div>
+        </div>
+        <hr id="last-hr"></hr>
+        <footer>
+          <p>
+            This project was coded by {""}
+            <a
+              href="https://www.shecodes.io/graduates/122713-jinoveva-lopes"
+              target="_blank"
+            >
+              Jinoveva Lopes
+            </a>{" "}
+            and is open-sourced on {""}
+            <a
+              href="https://github.com/Jinoveva/react-weather-app"
+              target="_blank"
+            >
+              Github
+            </a>{" "}
+            {""}
+            and hosted on {""}
+            <a
+              href="https://jinovevas-react-weather-app.netlify.app/"
+              target="_blank"
+            >
+              Netlify
+            </a>
+          </p>
+        </footer>
       </div>
-      <div>☁️10°C</div>
     </div>
   );
 }
