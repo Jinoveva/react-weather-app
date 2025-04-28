47177f612314240257f340b190eef7b57561c5e0
Jinoveva <jinovevalopes@gmail.com>
2025-04-28 13:18:31 +0200

    Forecast layout

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,38 +15,7 @@ export default function WeatherForecast(props) {
             <span className="WeatherForecast-temperature-min"> 10°</span>
           </div>
         </div>
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <WeatherIcon code="01d" size={36} />
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max">19°</span> |
-            <span className="WeatherForecast-temperature-min"> 10°</span>
-          </div>
-        </div>
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <WeatherIcon code="01d" size={36} />
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max">19°</span> |
-            <span className="WeatherForecast-temperature-min"> 10°</span>
-          </div>
-        </div>
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <WeatherIcon code="01d" size={36} />
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max">19°</span> |
-            <span className="WeatherForecast-temperature-min"> 10°</span>
-          </div>
-        </div>
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <WeatherIcon code="01d" size={36} />
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max">19°</span> |
-            <span className="WeatherForecast-temperature-min"> 10°</span>
-          </div>
-        </div>
+        
       </div>
     </div>
   );
